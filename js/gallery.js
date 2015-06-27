b026e7dceb444fd82f9cdfbd94f3ca631b4a0a77
Omar Al-Ithawi <i@omardo.com>
2015-06-27 14:41:27 +0300

    Finally it works fine on Chrome

--- js/gallery.js
+++ js/gallery.js
@@ -1,49 +1,84 @@
-;(function ($) {
-  var gallery = function () {
-    var $elem = $(this);
-    var activatedImage
+;(function() {
+  var utils = {
+    getElementIndex: function (elem) {
+      var siblings = elem.parentElement.children
 
-    var activateImage = function (index) {
-      if (!$elem.find('.thumb').eq(index).length) {
+      for (var i=0; i < siblings.length; i+=1) {
+        if (siblings[i] === elem) {
+          return i;
+        }
+      }
+
+      return -1;
+    },
+    addClassToElement: function(elem, value) {
+      var rspaces = /\s+/;
+      var classNames = (value || "").split(rspaces);
+      var className = " " + elem.className + " ",
+        setClass = elem.className;
+      for (var c = 0, cl = classNames.length; c < cl; c++) {
+        if (className.indexOf(" " + classNames[c] + " ") < 0) {
+          setClass += " " + classNames[c];
+        }
+      }
+      elem.className = setClass.replace(/^\s+|\s+$/g, ''); //trim
+    },
+    removeClassFromElement: function(elem, value) {
+      var rspaces = /\s+/;
+      var rclass = /[\n\t]/g
+      var classNames = (value || "").split(rspaces);
+      var className = (" " + elem.className + " ").replace(rclass, " ");
+      for (var c = 0, cl = classNames.length; c < cl; c++) {
+        className = className.replace(" " + classNames[c] + " ", " ");
+      }
+      elem.className = className.replace(/^\s+|\s+$/g, ''); //trim
+    }
+  };
+
+  var gallery = function(elem) {
+    var activeImageIndex = 0;
+    var thumbs = elem.querySelectorAll('.thumb');
+    var images = elem.querySelectorAll('.image');
+
+    var activateImage = function(index) {
+      if (!thumbs[index]) {
         return;
       }
 
-      $elem.find('.thumb').removeClass('active');
-      $elem.find('.thumb').eq(index).addClass('active');
+      activeImageIndex = index;
 
-      $elem.find('.image').removeClass('active');
-      $elem.find('.image').eq(index).addClass('active');
+      var toggleActiveClass = function(el, i) {
+        if (i === index) {
+          utils.addClassToElement(el, 'active');
+        } else {
+          utils.removeClassFromElement(el, 'active');
+        }
+      };
 
-      return;
+      for (var i=0; i < thumbs.length; i+=1) {
+        toggleActiveClass(thumbs[i], i);
+        toggleActiveClass(images[i], i);
+      }
     };
 
-    var imagesEl = $elem.find('.images')[0];
-    var imagesHammer = new Hammer(imagesEl);
 
-    var getActiveImageIndex = function () {
-      var $images = $elem.find('.image');
-      return $images.index('.active');
-    };
+    var imagesHammer = new Hammer(elem.querySelector('.images'));
 
-    /*
-    imagesHammer.on('swipeleft', function (e) {
-      e.gesture.preventDefault();
-      console.log('swipeleft', getActiveImageIndex(), '- 1');
-      activateImage(getActiveImageIndex() - 1);
-    });
-    */
-
-    imagesHammer.on('swiperight', function (e) {
-      e.preventDefault();
-      console.log('swiperight', getActiveImageIndex(), '+ 1');
-      activateImage(getActiveImageIndex() + 1);
+    imagesHammer.on('swipeleft', function(e) {
+      activateImage(activeImageIndex + 1);
     });
 
-    $elem.on('click', '.thumb', function () {
-      var index = $(this).index();
-      return activateImage(index);
+    imagesHammer.on('swiperight', function(e) {
+      activateImage(activeImageIndex - 1);
     });
+
+
+    for (var i=0; i < thumbs.length; i+=1) {
+      thumbs[i].addEventListener('click', function () {
+        return activateImage(utils.getElementIndex(this));
+      }, false);
+    }
   };
 
-  $.fn.gallery = gallery;
-}(window.$));
+  window.Gallery = gallery;
+}());
--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
-$(function () {
-  $('.gallery').gallery();
-});
+Gallery(document.getElementById('mygallery'));
